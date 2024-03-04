5ed9642444110f0a500924265dba0fe5fbeb4a5a
Lunessel <varloky@gmail.com>
2024-03-04 04:22:47 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import {useEffect} from "react";
 import eruda from "eruda";
-import {image} from "./p.jpg";
 const tg = window.Telegram.WebApp;
 
 
@@ -34,7 +33,6 @@ function App() {
             <p>This is web app for tg</p>
             <input type={"file"}/>
             <button onClick={onClose}>Close</button>
-            <img src={image}/>
         </div>
     );
 }
