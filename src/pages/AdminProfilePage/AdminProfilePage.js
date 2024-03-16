c63dd6d67b2d76a30e5882c331e099ebba65afd2
Lunessel <varloky@gmail.com>
2024-03-16 15:17:34 +0200

    init

--- src/pages/AdminProfilePage/AdminProfilePage.js
+++ src/pages/AdminProfilePage/AdminProfilePage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ChannelItem from "../../features/ChannelItem/ChannelItem";
 import Navigation from "../../features/Navigation/Navigation";
 
+import './admin_profile_page.scss'
+
 const AdminProfilePage = () => {
     return (
         <div className={"admin-page"}>
