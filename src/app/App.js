2c950ef686d4248b5c489ec55dc302aedb95d058
Lunessel <varloky@gmail.com>
2024-03-16 18:38:46 +0200

    init

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {useEffect} from "react";
 import eruda from "eruda";
 import AdminProfilePage from "../pages/AdminProfilePage/AdminProfilePage";
+import {Helmet} from "react-helmet";
 const tg = window.Telegram.WebApp;
 
 
@@ -32,7 +33,11 @@ function App() {
 
     return (
         <div className={"app"}>
+            <Helmet>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+            </Helmet>
             <AdminProfilePage />
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit magnam modi, non nostrum officiis tempora. Eveniet explicabo ipsa molestias non officia. Aliquam beatae doloremque molestias placeat quae quasi repudiandae veniam.
         </div>
     );
 }
